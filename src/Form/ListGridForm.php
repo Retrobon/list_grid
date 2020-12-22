613ecad95dcc04d5cccd723d42251113328c6397
Kirill <k@shatokhin.dev>
2020-12-22 18:53:34 +0200

    build config form

--- src/Form/ListGridForm.php
+++ src/Form/ListGridForm.php
@@ -62,16 +62,94 @@ class ListGridForm extends ConfigFormBase {
       '#type' => 'select',
       '#title' => $this->t('grid'),
       '#options' => $views_on_site,
-      '#default_value' => $config->get('views'),
+      '#default_value' => $config->get('grid'),
+      "#empty_option" => t('- Select -'),
     ];
 
     $form['views_list'] = [
       '#type' => 'select',
       '#title' => $this->t('list'),
       '#options' => $views_on_site,
-      '#default_value' => $config->get('views'),
+      '#default_value' => $config->get('list'),
+      '#states' => [
+        'invisible' => [
+          ':input[name="views_grid"]' => ['value' => ''],
+        ],
+      ],
     ];
+//https://gist.github.com/leymannx/72d41cf0baa4dee62d6ddc89bc7c7a5a
+    $set_id = $form_state->get('set_id');
+    if ($set_id === NULL) {
+      $set_id = $form_state->set('set_id', 0);
+      $set_id = 0;
+    }
+
+    $form['setIds'] = [
+      '#type' => 'fieldset',
+      '#prefix' => '<div id="set-id-wrapper">',
+      '#suffix' => '</div>',
+    ];
+
+
+    $form['setIds']['actions']['add_set'] = [
+      '#type' => 'submit',
+      '#limit_validation_errors' => [],
+      '#value' => t('add set views'),
+      '#submit' => ['::addOneSet'],
+      '#ajax' => [
+        'callback' => '::addmoreCallbackSet',
+        'wrapper' => 'set-id-wrapper',
+      ],
+      '#attributes' => ['class' => ['btn-transparent']],
+    ];
+
+
+    $form['setIds']['actions']['remove_set'] = [
+      '#type' => 'submit',
+      '#limit_validation_errors' => [],
+      '#value' => t('delete set views'),
+      '#submit' => ['::removeoneSet'],
+      '#ajax' => [
+        'callback' => '::addmoreCallbackSet',
+        'wrapper' => 'set-id-wrapper',
+      ],
+      '#name' => t('setremove'),
+      '#attributes' => ['class' => ['btn-transparent']],
+      '#suffix' => t('<div class="id-wrapper"></div>'),
+    ];
+
+    for ($i = 0; $i < $set_id; $i++) {
+      $form['setIds']['views_grid_' . $i] = [
+        '#type' => 'select',
+        '#title' => $this->t('grid'),
+        '#options' => $views_on_site,
+        '#default_value' => $config->get('grid_' . $i),
+        "#empty_option" => t('- Select -'),
+      ];
+
+      $form['setIds']['views_list_' . $i] = [
+        '#type' => 'select',
+        '#title' => $this->t('list'),
+        '#options' => $views_on_site,
+        '#default_value' => $config->get('list_' . $i),
+        "#empty_option" => t('- Select -'),
+      ];
+    }
+
+
     return parent::buildForm($form, $form_state);
+  }
+
+  public function addmoreCallbackSet(array &$form, FormStateInterface $form_state) {
+    $set_id = $form_state->get('set_id');
+    return $form['setIds'];
+  }
+
+  public function addOneSet(array &$form, FormStateInterface $form_state) {
+    $set_id = $form_state->get('set_id');
+    $add_button = $set_id + 1;
+    $form_state->set('set_id', $add_button);
+    $form_state->setRebuild();
   }
 
   /**
@@ -79,9 +157,21 @@ class ListGridForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
+    $count = 0;
+    $views = [];
+    foreach ($form_state->getValues() as $key => $value) {
+      if (strpos ( $key , 'views_grid_') === 0){
+        $views[$count]['grid'] = $value;
+      }
+      if (strpos ( $key , 'views_list_') === 0){
+        $views[$count]['list'] = $value;
+        $count++;
+      }
+    }
 
     $this->config('list_grid.listgrid')
       ->set('default', $form_state->getValue('default'))
+      ->set('views', $views)
       ->set('grid', $form_state->getValue('views_grid'))
       ->set('list', $form_state->getValue('views_list'))
       ->save();
--- src/ListGridService.php
+++ src/ListGridService.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\list_grid;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\TempStore\PrivateTempStoreFactory;
 
 
@@ -12,20 +13,27 @@ class ListGridService {
    */
   private $temp_store_factory;
 
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private $configFactory;
 
   /**
    * ListGridService constructor.
    *
    * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    */
-  public function __construct(PrivateTempStoreFactory $temp_store_factory) {
-
+  public function __construct(PrivateTempStoreFactory $temp_store_factory, ConfigFactoryInterface $configFactory) {
     $this->temp_store_factory = $temp_store_factory->get('grid_list');
+    $this->configFactory = $configFactory->get('list_grid.listgrid');
   }
 
   public function data() {
-    $config = \Drupal::config('list_grid.listgrid');
-    return explode(':', $config->get($this->temp_store_factory->get('grid_list')));
+    return [
+      explode(':', $this->configFactory->get($this->temp_store_factory->get('grid_list'))),
+      $this->configFactory->get()
+    ];
 
   }
 }
